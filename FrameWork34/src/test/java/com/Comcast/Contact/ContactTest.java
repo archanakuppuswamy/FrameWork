02bd65e9630c82e393b76e82bcc2f0dc2ca06400
SuRArC <SuRArC@SURYA------>
2022-05-12 10:41:51 +0530

    third commit

---
+++ FrameWork34/src/test/java/com/Comcast/Contact/ContactTest.java
@@ -0,0 +1,22 @@
+package com.Comcast.Contact;
+
+import org.testng.annotations.Test;
+//hii vijay
+public class ContactTest {
+	@Test(groups="smokeTest")
+	public void createContact() {
+		System.out.println("contact is created");
+		String value1= System.getProperty("browser");
+		System.out.println(value1);
+	}
+	//SDET34
+	@Test(groups="regressionTest")
+	public void modifyContact() {
+		System.out.println("contact is modified");
+	}
+	//AUTOMATION ENGG 1
+	@Test(groups="regressionTest")
+	public void deleteContact() {
+		System.out.println("contact is deleted");
+	}
+	}
